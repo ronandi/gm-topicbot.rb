52810a6c74b6ae365118c95cf87fbe0d7c05d8fc
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-10-18 16:28:58 -0400

    switch to chatbot

--- bot.rb
+++ bot.rb
@@ -1,20 +1,14 @@
+require 'chatbot'
 require 'sinatra'
-require 'net/http'
-require 'json'
-
-BOT_ID = ENV['GM_BOT_ID'];
-API_KEY = ENV['GM_API_KEY'];
-URI = 'https://api.groupme.com/v3/bots/post';
+Chatbot.configure do |config|
+  config.bot_id = ENV['GM_BOT_ID'];
+end
 
 post '/' do
-  message = validate_message(request.body.read)
-  send_message(message.topic_message) if message.is_topic_message?
+  Chatbot.processMessage(request.body.read)
 end
 
-def validate_message(msg)
-  Message.new((JSON.parse(msg) rescue {})) #If an exception occurs parsing, use {}
-end
-
-def send_message(msg)
-  Net::HTTP.post_form(URI, { "bot_id" => BOT_ID, "text" => msg })
+Chatbot.command '!topic' do |message|
+  return nil if message.message.nil?
+  return "#{message.sender} set topic to  #{message.message}"
 end
