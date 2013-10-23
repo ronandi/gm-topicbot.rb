7e894918feb84f06d91fbd57d823a20dc1a11df3
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-10-23 15:35:44 -0400

    Truncate topic titles in index

--- models/topic.rb
+++ models/topic.rb
@@ -1,9 +1,10 @@
 BASE_URI = "topicbot.herokuapp.com"
+TOPIC_CHARS = 160
 
 class Topic
   include DataMapper::Resource
   property :id, Serial
-  property :topic, String, :length => 160
+  property :topic, Text
   property :creator, String
   property :created_at, DateTime
 
@@ -11,5 +12,9 @@ class Topic
 
   def url
     return "#{BASE_URI}/topic/#{self[:id]}"
+  end
+
+  def shortened_topic
+    self[:topic].length < TOPIC_CHARS ? self[:topic] : self[:topic].slice(0, TOPIC_CHARS).concat('...')
   end
 end
